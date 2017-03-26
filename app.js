b38786704b28da61576602b73f13fd874cc6effc
slam24 <luismartinez242011@gmail.com>
2017-03-26 17:25:23 -0600

    feat: Method child

--- app.js
+++ app.js
@@ -10,10 +10,29 @@
 
 	//Obtener elemento
 	const preObject = document.getElementById('objeto');
+	const ulList = document.getElementById('list');
 	//Crear referencia
 	var dbRef1 = firebase.database().ref().child('objeto');
+	var dbRef2 = dbRef1.child('habilidades');
 	//Sicronizar cambios objecto
 	dbRef1.on('value', snap => {
 		preObject.innerText = JSON.stringify(snap.val(),null,3);
+	});
+	//Sincronizar cambios en la lista
+	dbRef2.on('child_added', snap => {
+		const li = document.createElement('li');
+		li.innerText = snap.val();
+		li.id = snap.key;
+		ulList.appendChild(li);
+	});
+
+	dbRef2.on('child_changed', snap => {
+		const liChanged = document.getElementById('snap.key');
+		liChanged.innerText = snap.val();
+	});
+
+	dbRef2.on('child_removed', snap => {
+		const liRemove = document.getElementById('snap.key');
+		liRemove.remove();
 	});
 }());
