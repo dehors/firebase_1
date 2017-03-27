f75f4107edbdd60d31fca5b4340c26c2793cd53c
slam24 <luismartinez242011@gmail.com>
2017-03-26 19:08:00 -0600

    feat: Auth

--- app.js
+++ app.js
@@ -35,4 +35,52 @@
 		const liRemove = document.getElementById('snap.key');
 		liRemove.remove();
 	});
+
+	//Auth
+	const txtEmail = document.getElementById('txtEmail');
+	const txtPassword = document.getElementById('txtPassword');
+	const btnLogin = document.getElementById('btnLogin');
+	const btnSingUp = document.getElementById('btnSingUp');
+	const btnLogOut = document.getElementById('btnLogOut');
+	//Evento login
+	btnLogin.addEventListener('click', e =>{
+		const email = txtEmail.value;
+		const pass = txtPassword.value;
+		const auth = firebase.auth();
+
+		const promise = auth.signInWithEmailAndPassword(email,pass);
+		promise.catch(e => console.log(e.message));
+	});
+
+	btnSingUp.addEventListener('click', e =>{
+		const email = txtEmail.value;
+		const pass = txtPassword.value;
+		const auth = firebase.auth();
+
+		const promise = auth.createUserWithEmailAndPassword(email,pass);
+		promise.catch(e => console.log(e.message));
+	});
+
+	btnSingUp.addEventListener('click', e =>{
+		const email = txtEmail.value;
+		const pass = txtPassword.value;
+		const auth = firebase.auth();
+
+		const promise = auth.createUserWithEmailAndPassword(email,pass);
+		promise.catch(e => console.log(e.message));
+	});
+
+	btnLogOut.addEventListener('click', e =>{
+		firebase.auth().signOut();
+	});
+
+	firebase.auth().onAuthStateChanged( firebaseUser => {
+		if (firebaseUser) {
+			console.log(firebaseUser);
+			btnLogOut.classList.remove('hide');
+		}else{
+			console.log('no logeuado');
+			btnLogOut.classList.add('hide');
+		}
+	});
 }());
